35c239797e0d0859466e4311dcc3a2a9e3fcbc5c
Anarcho Cat <d2717p@gmail.com>
2024-08-30 14:44:08 +0900

    add and use skill icons

--- components/SkillIcon.vue
+++ components/SkillIcon.vue
@@ -1,10 +1,14 @@
 <template>
-  <!-- <img src="" :alt="'スキル：' + name" width="64" height="64"> -->
-  <div class="w-16 h-16 bg-red-400 rounded-full"></div>
+  <img :src="file[name]" :alt="'スキル：' + name" width="64" height="64" class="rounded-full">
 </template>
 
 <script setup lang="ts">
+import iconZhuiji from '~/assets/img/skill/zhuiji.png';
 defineProps<{
   name: string
 }>();
+const file: Record<string, string> = {
+  'スキル１': iconZhuiji,
+  'スキル２': iconZhuiji,
+};
 </script>
