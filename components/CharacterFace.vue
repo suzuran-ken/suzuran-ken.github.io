4f5b10ac1024282b5677678fc7db699135f9cd3b
Anarcho Cat <d2717p@gmail.com>
2024-08-31 15:39:15 +0900

    move character image to public

---
+++ components/CharacterFace.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="w-24 md:w-32">
+    <NuxtImg width="128" height="128" sizes="84px md:128px" :src></NuxtImg>
+  </div>
+</template>
+<script setup lang="ts">
+const prop = defineProps<{
+  name: string
+}>();
+const file: Record<string, string> = {
+  'シモーナ': '/img/character/dot/character_hknightsicemagefset01.png'
+};
+const src = file[prop.name];
+</script>
--- pages/character/index.vue
+++ pages/character/index.vue
@@ -1,7 +1,12 @@
 <template>
   <ContentList v-slot="{ list }">
     <ul v-for="character in list" :key="character._path">
-      <li><a :href="character._path">{{ character['名前'] }}</a></li>
+      <li>
+        <NuxtLink :href="character._path" class="flex flex-col gap-2 items-center border bg-gray-100 w-fit hover:bg-gray-200">
+          <CharacterFace :name="character['名前']" />
+          <p>{{ character['名前'] }}</p>
+        </NuxtLink>
+      </li>
     </ul>
   </ContentList>
 </template>
