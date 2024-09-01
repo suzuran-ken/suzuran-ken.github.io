2c05351e4e1c42a2a963955f36aa4efe47c90993
Anarcho Cat <d2717p@gmail.com>
2024-09-01 13:43:03 +0900

    create gear icon component

--- components/GearIcon.vue
+++ components/GearIcon.vue
@@ -1,8 +1,15 @@
 <template>
-
+  <div>
+    <NuxtImg :src="`/img/gear/${src}`" width="256" height="512" :sizes />
+  </div>
 </template>
 <script setup lang="ts">
-defineProps<{
-  name: string
+const prop = defineProps<{
+  name: string,
+  sizes: string
 }>();
+const file: Record<string, string> = {
+
+};
+const src = file[prop.name];
 </script>
